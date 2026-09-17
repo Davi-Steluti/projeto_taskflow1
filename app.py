a3ef5f2cddef7301e0f49639535967685c7f95a2
senai_26_27 <davi.steluti@edu.senai.br>
2026-09-17 12:30:17 -0300

    Commit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, redirect, url_for
 #Criar objeto flask "apelido - app"
 app = Flask(__name__)
 lista = []
+lista_pessoas = []
 
 #-------------------------------------
 #Rotas
@@ -51,7 +52,27 @@ def listar_atividades():
 
 @app.route('/pessoa')
 def pessoa():
-    return render_template('pessoa.html')
+    return render_template('pessoa.html', dados_pessoas=lista_pessoas)
+
+
+@app.route('/pessoa/criar', methods=['GET', 'POST'])
+def criar_pessoa():
+    if request.method == 'POST':
+        nome_pessoa = request.form.get('nome')
+        email = request.form.get('email')
+        senha = request.form.get('senha')  # geralmente não é getlist para senha única
+        data_nascimento = request.form.get('data_nascimento')
+
+        dados_pessoa = {
+            'nome': nome_pessoa,
+            'email': email,
+            'senha': senha,
+            'data_nascimento': data_nascimento
+        }
+
+        lista_pessoas.append(dados_pessoa)
+        return redirect(url_for('pessoa', dados_pessoas=lista_pessoas))  # ou render_template adequado
+    return render_template('criar_pessoa.html')
 
 
 @app.route('/atividades/excluir/<int:indice>')
@@ -61,6 +82,12 @@ def excluir_atividade(indice):
     except IndexError:
         pass
     return redirect(url_for('listar_atividades'))
+
+@app.route('/pessoa/excluir/<int:indice>')
+def excluir_pessoa(indice):
+    if 0 <= indice < len(lista_pessoas):
+        lista_pessoas.pop(indice)
+    return redirect(url_for('pessoa'))
 
 # Iniciar aplicação web
 if __name__ == '__main__':
